adf2fae36d589f5293281be614b3e9e4ae444230
alrifaige1 <asmaalrifai02@gmail.com>
2024-04-13 10:10:34 +0200

    partie signup et login

--- painterest/base_app/urls.py
+++ painterest/base_app/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.test, name="main"),
-    path("home/", views.home, name="homes")
+    path("home/", views.home, name="homes"),
+    path('signup/', views.signup, name='signup'),  # Définition de l'URL nommée 'signup'
+    path('logout/', views.deconnexion, name='logout') # Définition de l'URL nommée 'logout'
 ]
--- painterest/base_app/views.py
+++ painterest/base_app/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from .models import UsersDB
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import logout
 
 # Create your views here.
 def test(request):
@@ -8,3 +10,19 @@ def test(request):
 def home(request):
     items = UsersDB.objects.all()
     return render(request, "home.html", {"elements": items})
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Rediriger l'utilisateur vers une page spécifique après inscription
+            return redirect('inscription_reussie')  # Rediriger vers une page nommée 'inscription_reussie'
+    else:
+        form = UserCreationForm()
+    return render(request, 'index_signup.html', {'form': form})
+
+def deconnexion(request):
+    logout(request)  # Déconnexion de l'utilisateur
+    return redirect('home')  # Rediriger l'utilisateur vers la page d'accueil 
